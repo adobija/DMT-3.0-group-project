e5bf3208c937db933e8206c384f30a847948d1a4
Michal.Kasinski <michal.kasinski@lo5.bielsko.pl>
2024-05-25 11:21:27 +0200

    add spring folder structure

---
+++ bankingapp/src/main/java/com/dmt/bankingapp/BankingappApplication.java
@@ -0,0 +1,13 @@
+package com.dmt.bankingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingappApplication.class, args);
+	}
+
+}
