cd432788031ca59b76735f21b220c00306e13698
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-05-29 11:16:52 +0200

    Fail test for timestamp

--- bankingapp/src/test/java/com/dmt/bankingapp/TransactionFeatureTest.java
+++ bankingapp/src/test/java/com/dmt/bankingapp/TransactionFeatureTest.java
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -53,6 +55,37 @@ public class TransactionFeatureTest {
         assertEquals(amountInTransaction, foundReceiverAccount.getAccountBalance());
 
 
+    }
+
+    @Test
+    public void testForDateAndTime(){
+        //Arrange
+        String giverClient = "userFirst";
+        String receiverClient = "userSecond";
+
+        User user1 = new User(giverClient, false, "test123");
+        User user2 = new User(receiverClient, false, "SuperCoolTest");
+
+        String giverAccountNumber = "222111222";
+        String receiverAccountNumber = "111333999";
+
+        Account giverAccount = new Account(giverAccountNumber, "savings", user1);
+        Account receiverAccount = new Account(receiverAccountNumber, "savings", user2);
+        double amount = 500.0;
+        giverAccount.setAccountBalance(amount, false);
+        entityManager.persist(giverAccount);
+        entityManager.persist(receiverAccount);
+
+        double amountInTransaction = 300.0;
+        Transaction transaction = new Transaction(giverAccount, receiverAccount, amountInTransaction);
+        entityManager.persist(transaction);
+        String now = LocalDateTime.now().toString().split(".")[0];
+        //Act
+        Transaction transaction1 = entityManager.find(Transaction.class, transaction.getTransactionID());
+
+        String timestampFromTransactionRecord = transaction1.getTimestamp().toString().split(".")[0];
+        //Assert
+        assertEquals(now, timestampFromTransactionRecord);
     }
 
 }
