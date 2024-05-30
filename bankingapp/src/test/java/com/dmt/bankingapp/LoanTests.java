e48b7cdfe8ebcadcfd0c63184dd5c5a21b3fe22b
artujo <152692817+artujo@users.noreply.github.com>
2024-05-30 17:07:27 +0200

    LoanTests - added tests for constructor, getters and setters, method 'IntrestAmount', clarified code.

--- bankingapp/src/test/java/com/dmt/bankingapp/LoanTests.java
+++ bankingapp/src/test/java/com/dmt/bankingapp/LoanTests.java
@@ -12,6 +12,9 @@ import jakarta.persistence.EntityManager;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
 
 @DataJpaTest
 @Transactional
@@ -21,7 +24,76 @@ public class LoanTests {
     private EntityManager entityManager;
 
     @Test
-    public void testLoans() {
+    public void testConstructor() {
+        // Arrange
+        Account loanAccount = new Account("loanAccNum", "loan", new User("loanTaker", false, "abc123"));
+        Account checkingAccount = new Account("checkingAccNum", "checking", new User("loanTaker", false, "abc123"));
+        Account bankAccount = new Account("bankAccNum", "bank", new User("bankName", false, "1234"));
+        double principalAmount = 50000.0;
+        double interestRate = 2.9;
+        int loanDuration = 12;
+
+        // Act
+        Loan loan = new Loan(loanAccount, checkingAccount, principalAmount, interestRate, loanDuration, bankAccount);
+
+        // Assert
+        assertEquals(loanAccount, loan.getLoanAccount());
+        assertEquals(checkingAccount, loan.getCheckingAccount());
+        assertEquals(bankAccount, loan.getBankAccount());
+        assertEquals(principalAmount, loan.getPrincipalLoanAmount());
+        assertEquals(interestRate, loan.getIntrestRate());
+        assertEquals(loanDuration, loan.getLoanDuration());
+        assertNotNull(loan.getUser());
+        assertEquals(loan.getUser(), checkingAccount.getUser());
+    }
+
+    @Test
+    public void testGettersAndSetters() {
+        // Arrange
+        Loan loan = new Loan();
+        int loanID = 1;
+        double principalAmount = 300000.0;
+        double totalAmount = 12000.0;
+        int loanDuration = 360;
+        double interestRate = 6.5;
+        LocalDateTime timestamp = LocalDateTime.now();
+
+        // Act
+        loan.setLoanID(loanID);
+        loan.setPrincipalLoanAmout(principalAmount);
+        loan.setTotalLoanAmout(totalAmount);
+        loan.setLoanDuration(loanDuration);
+        loan.setIntrestRate(interestRate);
+        loan.setTimestamp(timestamp);
+
+        // Assert
+        assertEquals(loanID, loan.getLoanID());
+        assertEquals(principalAmount, loan.getPrincipalLoanAmount());
+        assertEquals(totalAmount, loan.getTotalLoanAmount());
+        assertEquals(loanDuration, loan.getLoanDuration());
+        assertEquals(interestRate, loan.getIntrestRate());
+        assertEquals(timestamp, loan.getTimestamp());
+    }
+
+    @Test
+    public void testIntrestAmount() {
+        // Arrange
+        double principalAmount = 10000.0;
+        double interestRate = 8.2;
+        int loanDuration = 120;
+
+        Loan loan = new Loan();
+        
+        // Act
+        double calculatedIntrest = loan.intrestAmount(principalAmount, interestRate, loanDuration);
+        double expectedIntrest = 8200.0;
+
+        // Assert
+        assertEquals(calculatedIntrest, expectedIntrest);
+    }
+
+    @Test
+    public void testGrantLoan() {
         // Arrange - instantiating a user and a bank, instantiating checking and loan account for the user, and account for the bank
         String loanTaker = "userTaker";
         String bankName = "bank";
