9d37c1091f701dd4f26939d53eaac15ecc6a4ac0
artujo <152692817+artujo@users.noreply.github.com>
2024-06-07 11:02:55 +0200

    Installment - added getters and setters

--- bankingapp/src/main/java/com/dmt/bankingapp/entity/Installment.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/entity/Installment.java
@@ -4,10 +4,62 @@ import jakarta.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "Installment")
+@Table(name = "Installments") // Use plural form for table names to be consistent
 public class Installment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "installmentID")
+    private int installmentID;
+
     @ManyToOne
-    @JoinColumn(name = "loan", referencedColumnName = "loanID")
+    @JoinColumn(name = "loanID", referencedColumnName = "loanID")
     private Loan loan;
+
+    @Column(name = "installmentAmount")
+    private double installmentAmount;
+
+    @Column(name = "dueDate")
+    private LocalDateTime dueDate;
+
+    public Installment() {
+    }
+
+    public Installment(Loan loan, double installmentAmount, LocalDateTime dueDate) {
+        this.loan = loan;
+        this.installmentAmount = installmentAmount;
+        this.dueDate = dueDate;
+    }
+
+    public int getInstallmentID() {
+        return installmentID;
+    }
+
+    public void setInstallmentID(int installmentID) {
+        this.installmentID = installmentID;
+    }
+
+    public Loan getLoan() {
+        return loan;
+    }
+
+    public void setLoan(Loan loan) {
+        this.loan = loan;
+    }
+
+    public double getInstallmentAmount() {
+        return installmentAmount;
+    }
+
+    public void setInstallmentAmount(double installmentAmount) {
+        this.installmentAmount = installmentAmount;
+    }
+
+    public LocalDateTime getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(LocalDateTime dueDate) {
+        this.dueDate = dueDate;
+    }
 }
