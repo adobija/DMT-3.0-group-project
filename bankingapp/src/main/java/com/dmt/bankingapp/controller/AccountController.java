d6542a50872f168ac1c4078dba1690f7a3d8903c
artujo <152692817+artujo@users.noreply.github.com>
2024-06-10 19:59:27 +0200

    AccountController - implementing method to create account number

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/AccountController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/AccountController.java
@@ -3,6 +3,8 @@ package com.dmt.bankingapp.controller;
 import com.dmt.bankingapp.entity.Account;
 import com.dmt.bankingapp.entity.Client;
 import com.dmt.bankingapp.repository.AccountRepository;
+import com.dmt.bankingapp.utils.AccountNumberGenerator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,11 +18,42 @@ public class AccountController {
     private AccountRepository accountRepository;
 
     @PostMapping("/add")
-    public @ResponseBody String addNewAccount(@RequestParam String accountNumber, @RequestParam Account.AccountType accountType, @RequestParam int clientId) {
-        // Client fetching and validation will be handled in the ClientController - to be created
-        Account account = new Account(accountNumber, accountType, new Client());
+    public @ResponseBody String addNewAccount(@RequestParam Account.AccountType accountType, @RequestParam int clientId) {
+        String accountNumber = ""; 
+        String generatedNumber = "";
+        while (accountNumber.isEmpty()){
+            generatedNumber = AccountNumberGenerator.generateAccountNumber();
+            if (accountRepository.findByAccountNumber(generatedNumber) == null) {
+                accountNumber = generatedNumber;
+            }
+        }
+        
+        Account account = new Account(accountNumber, accountType, new Client()); // Client fetching and validation will be handled in the ClientController - to be created
         accountRepository.save(account);
         return "Account created successfully";
+    }
+
+    @PostMapping("/updateBalance")
+    public @ResponseBody String updateBalance(@RequestParam int accountId, @RequestParam double amount, @RequestParam boolean isExpense) {
+        Account account = accountRepository.findById(accountId).orElse(null);
+        if (account != null) {
+            account.setAccountBalance(amount, isExpense);
+            accountRepository.save(account);
+            return "Account balance updated successfully";
+        } else {
+            return "Account not found";
+        }
+    }
+
+    @DeleteMapping("/remove")
+    public @ResponseBody String removeAccount(@RequestParam int accountId) {
+        Account account = accountRepository.findById(accountId).orElse(null);
+        if (account != null) {
+            accountRepository.delete(account);
+            return "Account deleted successfully";
+        } else {
+            return "Account not found";
+        }
     }
 
     @GetMapping("/all")
