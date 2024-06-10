4d657d99a423b109344c4ad15969345f9aa0e758
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-10 10:36:02 +0200

    Add possibility to find transactions by account of sender and receiver

--- bankingapp/src/main/java/com/dmt/bankingapp/repository/TransactionRepository.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/repository/TransactionRepository.java
@@ -4,6 +4,11 @@ import com.dmt.bankingapp.entity.Transaction;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TransactionRepository extends CrudRepository<Transaction, Integer> {
+    List<Transaction> findByAccountOfSender(Integer accountOfSender);
+    List<Transaction> findByAccountOfReceiver(Integer accountOfReceiver);
+
 }
