fd21d0d4a407579394850001a99fddb84e4fd200
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-10 10:27:25 +0200

    Add possibility to find Account by clientID

--- bankingapp/src/main/java/com/dmt/bankingapp/repository/AccountRepository.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/repository/AccountRepository.java
@@ -4,4 +4,5 @@ import com.dmt.bankingapp.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccountRepository extends CrudRepository<Account,Integer> {
+    Account findByClientId(Integer clientID);
 }
