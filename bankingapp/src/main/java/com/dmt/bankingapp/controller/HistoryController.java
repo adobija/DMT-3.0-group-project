5a662e7f473a8a83583b97b2f6ca91d91a122e0b
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-10 10:39:54 +0200

    Create endpoint for transaction history in checking account, validation of created code

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/HistoryController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/HistoryController.java
@@ -1,8 +1,14 @@
 package com.dmt.bankingapp.controller;
 
+import com.dmt.bankingapp.entity.Account;
+import com.dmt.bankingapp.entity.Client;
 import com.dmt.bankingapp.entity.Transaction;
+import com.dmt.bankingapp.repository.AccountRepository;
+import com.dmt.bankingapp.repository.ClientRepository;
+import com.dmt.bankingapp.repository.TransactionRepository;
 import com.dmt.bankingapp.service.implementation.DetailsOfLoggedClientImpl;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,18 +24,39 @@ public class HistoryController {
     @Autowired
     private final DetailsOfLoggedClientImpl detailsOfLoggedClientImp;
 
-    public HistoryController(DetailsOfLoggedClientImpl detailsOfLoggedClientImp) {
+    @Autowired
+    private final ClientRepository clientRepository;
+
+    @Autowired
+    private final AccountRepository accountRepository;
+
+    @Autowired
+    private final TransactionRepository transactionRepository;
+
+    public HistoryController(DetailsOfLoggedClientImpl detailsOfLoggedClientImp, ClientRepository clientRepository, AccountRepository accountRepository, TransactionRepository transactionRepository) {
         this.detailsOfLoggedClientImp = detailsOfLoggedClientImp;
+        this.clientRepository = clientRepository;
+        this.accountRepository = accountRepository;
+        this.transactionRepository = transactionRepository;
     }
     //Endpoint for history of checking account
     @GetMapping(path = "/checking")
-    public @ResponseBody List<Transaction> getHistoryOfIncomeAndExpense(HttpServlet request){
+    public @ResponseBody List<Transaction> getHistoryOfIncomeAndExpense(HttpServletRequest request){
         //Get name of logged client
-        //find all details of this client
+        String login = detailsOfLoggedClientImp.getNameFromClient(request);
+        //find instance of this client
+        Client client = clientRepository.findByClientName(login);
         //get ID of client
-        //get account of client
-        //find all details of client's account
+        Integer clientID = client.getClientID();
+        //find instance of client's account
+        Account account = accountRepository.findByClientId(clientID);
         //get ID of account
+        Integer accountId = account.getAccountID();
         //fetch all records in Transaction table that have ID of account
+        List<Transaction> incomeTransactions = transactionRepository.findByAccountOfReceiver(accountId);
+        List<Transaction> expenseTransactions = transactionRepository.findByAccountOfSender(accountId);
+
+        //return list of transactions
+        return incomeTransactions;
     }
 }
