d0481d36672f7c02a640087a09de2472c78e64a7
artujo <152692817+artujo@users.noreply.github.com>
2024-06-10 20:31:06 +0200

    ClientRepository updated to launch properly

---
+++ bankingapp/src/main/java/com/dmt/bankingapp/repository/ClientRepository.java
@@ -0,0 +1,10 @@
+package com.dmt.bankingapp.repository;
+
+import com.dmt.bankingapp.entity.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+    
+}
