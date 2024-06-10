101b882fbb8ceff9f1735cada3e43687ee711eee
artujo <152692817+artujo@users.noreply.github.com>
2024-06-10 19:38:39 +0200

    AccountController - adding new account

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/AccountController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/AccountController.java
@@ -1,9 +1,12 @@
 package com.dmt.bankingapp.controller;
 
+import com.dmt.bankingapp.entity.Account;
+import com.dmt.bankingapp.entity.Client;
 import com.dmt.bankingapp.repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/account")
@@ -12,4 +15,13 @@ public class AccountController {
     @Autowired
     private AccountRepository accountRepository;
 
+    @PostMapping("/add")
+    public @ResponseBody String addNewAccount(@RequestParam String accountNumber, @RequestParam Account.AccountType accountType, @RequestParam int clientId) {
+        // Client fetching and validation will be handled in the ClientController - to be created
+        Account account = new Account(accountNumber, accountType, new Client());
+        accountRepository.save(account);
+        return "Account created successfully";
+    }
+
+    
 }
