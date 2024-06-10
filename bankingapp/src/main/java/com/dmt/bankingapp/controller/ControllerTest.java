42a6d21e5d580c2c4d972ba3d52715c2ab1a0b8c
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-10 22:40:49 +0200

    Fix package name

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/ControllerTest.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/ControllerTest.java
@@ -1,4 +1,4 @@
-package com.dmt.bankingapp.Controller;
+package com.dmt.bankingapp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
