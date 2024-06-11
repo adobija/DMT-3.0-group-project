f35f024c43e48e515d09b259112d20b6b7d0422f
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-11 10:08:14 +0200

    Add type of transaction to increase readability

--- bankingapp/src/main/java/com/dmt/bankingapp/record/History.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/record/History.java
@@ -3,12 +3,13 @@ package com.dmt.bankingapp.record;
 import java.time.LocalDateTime;
 
 public record History(
+        String typeOf,
         String contributorName,
         String contributorAccountNumber,
         String amount,
         LocalDateTime dateOfTransaction,
-        Double balanceAfterTransaction,
         Integer transactionID
+
 ) {
 
 }
