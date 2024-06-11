9c94d46e17606214ad4d4cb5a7843f75dc67e0e0
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-11 14:55:34 +0200

    Create Test classes, work in progress

---
+++ bankingapp/src/test/java/com/dmt/bankingapp/RepositoriesTests.java
@@ -0,0 +1,4 @@
+package com.dmt.bankingapp;
+
+public class RepositoriesTests {
+}
---
+++ bankingapp/src/test/java/com/dmt/bankingapp/utils/BcryptPasswordGeneratorTests.java
@@ -0,0 +1,7 @@
+package com.dmt.bankingapp.utils;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BcryptPasswordGeneratorTests {
+
+}
