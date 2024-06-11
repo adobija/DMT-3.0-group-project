46037ef65d8077ee2c84aa8c4a1ef33c11421834
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-11 09:18:23 +0200

    Fix

--- bankingapp/src/main/java/com/dmt/bankingapp/repository/AccountRepository.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/repository/AccountRepository.java
@@ -1,10 +1,11 @@
 package com.dmt.bankingapp.repository;
 
 import com.dmt.bankingapp.entity.Account;
+import com.dmt.bankingapp.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Integer> {
-    Account findByClientId(int ClientId);
+    Account findByClient(Client client);
 }
--- bankingapp/src/main/java/com/dmt/bankingapp/repository/TransactionRepository.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/repository/TransactionRepository.java
@@ -1,5 +1,6 @@
 package com.dmt.bankingapp.repository;
 
+import com.dmt.bankingapp.entity.Account;
 import com.dmt.bankingapp.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    List<Transaction> findByAccountOfSender(Integer accountOfSender);
-    List<Transaction> findByAccountOfReceiver(Integer accountOfReceiver);
+    List<Transaction> findByGiver(Account giver);
+    List<Transaction> findByReceiver(Account receiver);
 
 }
