410b6602bfe6b1803ab3ddf0a6a439f3af42a483
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-12 10:37:01 +0200

    Add method to get whole instance of logged client

--- bankingapp/src/main/java/com/dmt/bankingapp/service/interfaceClass/DetailsOfLoggedClient.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/service/interfaceClass/DetailsOfLoggedClient.java
@@ -1,8 +1,10 @@
 package com.dmt.bankingapp.service.interfaceClass;
 
+import com.dmt.bankingapp.entity.Client;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Service;
 
 public interface DetailsOfLoggedClient {
     public String getNameFromClient(HttpServletRequest request);
+    public Client getLoggedClientInstance(HttpServletRequest request);
 }
