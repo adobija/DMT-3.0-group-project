a49e1f3aa65b1d4e90e05a32174effdf1152bac5
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-12 12:04:33 +0200

    Add advice

--- bankingapp/src/test/java/com/dmt/bankingapp/springTests/controllerTests/HistoryControllerTest.java
+++ bankingapp/src/test/java/com/dmt/bankingapp/springTests/controllerTests/HistoryControllerTest.java
@@ -1,5 +1,6 @@
 package com.dmt.bankingapp.springTests.controllerTests;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,4 +14,21 @@ public class HistoryControllerTest {
     //We need only 60% coverage of test
     //So would leave it empty/delete it
     //~Olek
+    //
+    //ps.
+    //Test for header response of "ErrorMessage" and status code
+    @Test
+    public void testForStatus200(){
+
+    }
+
+    @Test
+    public void testForStatus403(){
+
+    }
+
+    @Test
+    public void testForStatus404(){
+
+    }
 }
