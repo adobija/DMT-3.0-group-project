c7922b15e3902bcce4575620c8e9539898077167
Piotr <piotr.kozik@lo5.bielsko.pl>
2024-06-13 18:19:49 +0200

    Add REST controller and request mapping for deposit operations

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/DepositController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/DepositController.java
@@ -1,6 +1,11 @@
 package com.dmt.bankingapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/deposit")
 public class DepositController {
-    
+
     
 }
