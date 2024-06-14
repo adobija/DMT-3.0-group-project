9d38cfde0c6aa99c7ca83507e151e443c9af439e
artujo <152692817+artujo@users.noreply.github.com>
2024-06-14 11:05:10 +0200

    TransactionController - fetching history of all or given account only for admins

--- bankingapp/src/main/java/com/dmt/bankingapp/security/EndpointAccess.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/security/EndpointAccess.java
@@ -30,8 +30,8 @@ public class EndpointAccess {
                 .requestMatchers(HttpMethod.GET, "/account/byAccountNumber").authenticated()
                 //security for TransactionController
                 .requestMatchers(HttpMethod.POST, "/transaction/add").authenticated()
-                .requestMatchers(HttpMethod.GET, "/transaction/all").authenticated()
-                .requestMatchers(HttpMethod.GET, "/transaction/byAccountID").authenticated()
+                .requestMatchers(HttpMethod.GET, "/transaction/everyTransaction").authenticated()
+                .requestMatchers(HttpMethod.GET, "/transaction/byAccountNumber").authenticated()
                 //security for LoanController
                 .requestMatchers(HttpMethod.POST, "/loan/add").authenticated()
                 .requestMatchers(HttpMethod.GET, "/loan/all").authenticated()
