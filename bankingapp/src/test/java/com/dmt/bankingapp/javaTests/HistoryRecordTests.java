6cbf6e1580e2995c05b596ea473058bf3b898844
artujo <152692817+artujo@users.noreply.github.com>
2024-06-17 14:44:11 +0200

    DecimalPlacesAdjusterTest enhanced

--- bankingapp/src/test/java/com/dmt/bankingapp/javaTests/HistoryRecordTests.java
+++ bankingapp/src/test/java/com/dmt/bankingapp/javaTests/HistoryRecordTests.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class HistoryRecordTests {
 
@@ -28,5 +29,28 @@ public class HistoryRecordTests {
         assertEquals(history.amount(), amount);
         assertEquals(history.dateOfTransaction(), dateOfTransaction);
         assertEquals(history.transactionID(), transactionID);
+    }
+
+    @Test
+    void testHistoryWithDifferentData() {
+        // Arrange
+        String typeOf = "Withdrawal";
+        String contributorName = "Johny";
+        String contributorAccountNumber = "testAccountNumber";
+        String amount = "500.00";
+        LocalDateTime dateOfTransaction = LocalDateTime.now().minusDays(1);
+        Integer transactionID = 2;
+
+        // Act
+        History history = new History(typeOf, contributorName, contributorAccountNumber, amount, dateOfTransaction, transactionID);
+
+        // Assert
+        assertNotNull(history);
+        assertEquals(typeOf, history.typeOf());
+        assertEquals(contributorName, history.contributorName());
+        assertEquals(contributorAccountNumber, history.contributorAccountNumber());
+        assertEquals(amount, history.amount());
+        assertEquals(dateOfTransaction, history.dateOfTransaction());
+        assertEquals(transactionID, history.transactionID());
     }
 }
