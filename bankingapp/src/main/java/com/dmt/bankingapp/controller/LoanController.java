4b69b3d8fb8192f17ae21b601f028c2b7577c111
artujo <152692817+artujo@users.noreply.github.com>
2024-06-18 22:16:24 +0200

    Formatted loan data when retrieving

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/LoanController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/LoanController.java
@@ -19,7 +19,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 @RestController
@@ -43,6 +45,8 @@ public class LoanController {
 
     @Autowired
     private DetailsOfLoggedClient detailsOfLoggedClient;
+
+    private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     @PostMapping("/add")
     public @ResponseBody String addNewLoan(@RequestParam double principalAmount,
@@ -108,23 +112,78 @@ public class LoanController {
     }
 
     @GetMapping("/all")
-    public @ResponseBody List<Loan> getAllLoans(HttpServletRequest request) {
+    public @ResponseBody String getAllLoans(HttpServletRequest request) {
         String requesterName = detailsOfLoggedClient.getNameFromClient(request);
         Client requester = clientRepository.findByClientName(requesterName);
         if(!requester.isAdmin()){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You don't have permission!");
         }
-        return loanRepository.findAll();
+
+        List<Loan> allLoans = loanRepository.findAll();
+        StringBuilder output = new StringBuilder();
+
+        for (Loan loan : allLoans) {
+            LocalDateTime dateOfLoan = loan.getDateOfLoan();
+            String formattedDate = dateOfLoan.format(formatter);
+
+            output.append(loan.getLoanID())
+                        .append(": ")
+                        .append(formattedDate)
+                        .append("  account: ")
+                        .append(loan.getLoanAccount().getAccountNumber())
+                        .append("  total: ")
+                        .append(loan.getTotalLoanAmount())
+                        .append("  left: ")
+                        .append(loan.getLeftToPay())
+                        .append("  client: ")
+                        .append(loan.getClient().getClientID())
+                        .append("\n");
+        }
+
+        // Remove the last newline in the output
+        if (output.length() > 0) {
+            output.setLength(output.length() - 1);
+        }
+        return output.toString();
     }
 
     @GetMapping("/byLoanId")
-    public @ResponseBody Loan getLoanById(@RequestParam int loanID, HttpServletRequest request) {
+    public @ResponseBody String getLoanById(@RequestParam int loanID, HttpServletRequest request) {
         String requesterName = detailsOfLoggedClient.getNameFromClient(request);
         Client requester = clientRepository.findByClientName(requesterName);
         if(!requester.isAdmin()){
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "You don't have permission!");
         }
-        Optional<Loan> loanOptional = loanRepository.findById(loanID);
-        return loanOptional.orElse(null);
+        Loan loan = loanRepository.findByLoanID(loanID);
+        if (loan == null) {
+            throw new NoSuchElementException("Loan with this ID does not exist!");
+        }
+
+        StringBuilder output = new StringBuilder();
+        LocalDateTime dateOfLoan = loan.getDateOfLoan();
+        String formattedDate = dateOfLoan.format(formatter);
+
+        output.append("loan ID: ")
+                .append(loan.getLoanID())
+                .append("\ndate: ")
+                .append(formattedDate)
+                .append("\naccount: ")
+                .append(loan.getLoanAccount().getAccountNumber())
+                .append("\ntotal: ")
+                .append(loan.getTotalLoanAmount())
+                .append("\nleft: ")
+                .append(loan.getLeftToPay())
+                .append("\nclient: ")
+                .append(loan.getClient().getClientID())
+                .append("\nduration: ")
+                .append(loan.getLoanDuration())
+                .append("\nintrest: ")
+                .append(loan.getInterestRate())
+                .append("\ncommision: ")
+                .append(loan.getCommisionRate())
+                .append("\nactive: ")
+                .append(loan.getIsActive());
+
+        return output.toString();
     }
 }
