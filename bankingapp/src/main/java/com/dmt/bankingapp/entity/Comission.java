beb5ca57ff95284ac904d4d9e6b0e55781d649c8
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-18 23:32:14 +0200

    Create entity for comission rates

---
+++ bankingapp/src/main/java/com/dmt/bankingapp/entity/Comission.java
@@ -0,0 +1,45 @@
+package com.dmt.bankingapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Comissions")
+public class Comission {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comissionId")
+    private Integer commisionId;
+
+    @Column(name = "comissionOf")
+    private String comissionOf;
+
+    @Column(name = "commissionRateInPercent")
+    private int commissionRateInPercent;
+
+    public Comission(int commissionRateInPercent, String comissionOf, Integer commisionId) {
+        this.commissionRateInPercent = commissionRateInPercent;
+        this.comissionOf = comissionOf;
+        this.commisionId = commisionId;
+    }
+
+    public Integer getCommisionId() {
+        return commisionId;
+    }
+
+    public String getComissionOf() {
+        return comissionOf;
+    }
+
+    public void setComissionOf(String comissionOf) {
+        this.comissionOf = comissionOf;
+    }
+
+    public int getCommissionRateInPercent() {
+        return commissionRateInPercent;
+    }
+
+    public void setCommissionRateInPercent(int commissionRateInPercent) {
+        this.commissionRateInPercent = commissionRateInPercent;
+    }
+}
