600f6587c0d076740d5054552a5a036a2d0e50ae
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-18 23:42:48 +0200

    Create comission rates on app start

--- bankingapp/src/main/java/com/dmt/bankingapp/BankingappApplication.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/BankingappApplication.java
@@ -2,8 +2,10 @@ package com.dmt.bankingapp;
 
 import com.dmt.bankingapp.entity.Account;
 import com.dmt.bankingapp.entity.Client;
+import com.dmt.bankingapp.entity.Comission;
 import com.dmt.bankingapp.repository.AccountRepository;
 import com.dmt.bankingapp.repository.ClientRepository;
+import com.dmt.bankingapp.repository.ComissionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,11 +14,6 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankingappApplication {
-
-    public BankingappApplication(ClientRepository clientRepository, AccountRepository accountRepository) {
-        this.clientRepository = clientRepository;
-        this.accountRepository = accountRepository;
-    }
 
     public static void main(String[] args) {
 		SpringApplication.run(BankingappApplication.class, args);
@@ -27,6 +24,15 @@ public class BankingappApplication {
 
 	@Autowired
 	private final AccountRepository accountRepository;
+
+	@Autowired
+	private final ComissionRepository comissionRepository;
+
+	public BankingappApplication(ClientRepository clientRepository, AccountRepository accountRepository, ComissionRepository comissionRepository) {
+		this.clientRepository = clientRepository;
+		this.accountRepository = accountRepository;
+		this.comissionRepository = comissionRepository;
+	}
 
 	@Bean
 	public CommandLineRunner commandLineRunner (){
@@ -43,6 +49,20 @@ public class BankingappApplication {
 				accountRepository.save(accountForDeposits);
 				accountRepository.save(accountForLoans);
 			}
+
+			Comission checkComissionForLoan = comissionRepository.findByComissionOf("LOAN");
+			Comission checkComissionForDeposit = comissionRepository.findByComissionOf("LOAN");
+
+			if(checkComissionForLoan == null){
+				Comission comissionForLoan = new Comission(10, "LOAN");
+				comissionRepository.save(comissionForLoan);
+			}
+			if(checkComissionForDeposit == null){
+				Comission comissionForDeposit = new Comission(10, "DEPOSIT");
+				comissionRepository.save(comissionForDeposit);
+			}
+
+
 		};
 	}
 }
