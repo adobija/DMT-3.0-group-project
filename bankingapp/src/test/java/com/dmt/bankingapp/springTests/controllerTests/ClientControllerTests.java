2c0d66a778120081af098599e6bb76761df9731a
Piotr <piotr.kozik@lo5.bielsko.pl>
2024-06-19 16:23:33 +0200

    Add testEditPasswordClientNotFound

--- bankingapp/src/test/java/com/dmt/bankingapp/springTests/controllerTests/ClientControllerTests.java
+++ bankingapp/src/test/java/com/dmt/bankingapp/springTests/controllerTests/ClientControllerTests.java
@@ -1,16 +1,21 @@
 package com.dmt.bankingapp.springTests.controllerTests;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mindrot.jbcrypt.BCrypt;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.ArgumentCaptor; // Add this import statement
+import org.springframework.http.HttpStatus;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.dmt.bankingapp.controller.ClientController;
 import com.dmt.bankingapp.entity.Client;
 import com.dmt.bankingapp.repository.ClientRepository;
@@ -56,5 +61,20 @@ public class ClientControllerTests {
         ArgumentCaptor<Client> clientArgumentCaptor = ArgumentCaptor.forClass(Client.class); // Declare and initialize
         verify(clientRepository).save(clientArgumentCaptor.capture());
         assertEquals(newName, clientArgumentCaptor.getValue().getClientName());
+    }
+
+    private MockHttpServletRequest request;
+    private String clientName = "ClientName";
+    private String newPassword = "newPassword";
+
+    @Test
+    void testEditPasswordClientNotFound() {
+        when(clientRepository.findByClientName(clientName)).thenReturn(null);
+    
+        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
+            clientController.editPassword(newPassword, request);
+        });
+    
+        assertEquals(HttpStatus.NOT_FOUND, exception.getStatusCode());
     }
 }
