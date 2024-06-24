8d844898ad333f852cafd5581f29cd9e2fe10cec
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-24 18:50:47 +0200

    Change controller to return view not just string

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/RegisterController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/RegisterController.java
@@ -6,11 +6,12 @@ import com.dmt.bankingapp.repository.ClientRepository;
 import com.dmt.bankingapp.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-@RestController
+@Controller
 @RequestMapping(path = "/register")
 public class RegisterController {
 
@@ -21,7 +22,7 @@ public class RegisterController {
     private AccountService accountService;
 
     @PostMapping("/newClient")
-    public @ResponseBody String addNewClient(@RequestParam String clientName, @RequestParam String clientPassword, Model model) {
+    public String addNewClient(@RequestParam String clientName, @RequestParam String clientPassword, Model model) {
         Client exists = clientRepository.findByClientName(clientName);
         if (exists != null) {
             throw new ResponseStatusException(HttpStatus.CONFLICT, "Client with this user name already exists - failed to create new client profile");
@@ -32,8 +33,8 @@ public class RegisterController {
             client.addAccount(accountService.getLatestAccount());
             client.setCheckingAccount(accountService.getLatestAccount());
             clientRepository.save(client);
-            model.addAttribute("return", "New client profile created successfully");
-            return "New client profile created successfully";
+            model.addAttribute("message", "New client profile created successfully");
+            return "registerTemplates/success";
         }
     }
 }
