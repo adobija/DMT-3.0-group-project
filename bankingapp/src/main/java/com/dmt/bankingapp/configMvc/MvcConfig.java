b07fb3728fb5bf82b2a0248bccc11e1759a80b4d
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-24 09:28:21 +0200

    Change directory

--- bankingapp/src/main/java/com/dmt/bankingapp/configMvc/MvcConfig.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/configMvc/MvcConfig.java
@@ -1,4 +1,4 @@
-package com.dmt.bankingapp;
+package com.dmt.bankingapp.configMvc;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
