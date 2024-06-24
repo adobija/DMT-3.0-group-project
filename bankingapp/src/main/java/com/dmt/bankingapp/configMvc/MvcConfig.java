5d02a8ba7912f72ad18890fd92ff2d89e943cde5
dmt-adobija <aleksander.dobija@lo5.bielsko.pl>
2024-06-24 09:41:20 +0200

    Setup mvcConfig file

--- bankingapp/src/main/java/com/dmt/bankingapp/configMvc/MvcConfig.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/configMvc/MvcConfig.java
@@ -8,15 +8,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
 
 	public void addViewControllers(ViewControllerRegistry registry) {
-		
+		//index site
 		registry.addViewController("/").setViewName("welcome");
-		registry.addViewController("/welcome").setViewName("welcome");
+		//registry.addViewController("/welcome").setViewName("welcome");
+
+		//register
 		registry.addViewController("/signup").setViewName("signup");
+		//login
 		registry.addViewController("/login").setViewName("login");
+
+
 		registry.addViewController("/homepage").setViewName("homepage");
-
 		registry.addViewController("/hello").setViewName("hello");
-
 		registry.addViewController("/admin").setViewName("admin");
 		
 
