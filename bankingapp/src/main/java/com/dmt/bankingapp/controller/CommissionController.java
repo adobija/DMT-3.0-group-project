244579bca619a155007434fa7305777da24538b7
artujo <152692817+artujo@users.noreply.github.com>
2024-06-26 07:59:42 +0200

    unused and doubled imports removed

--- bankingapp/src/main/java/com/dmt/bankingapp/controller/CommissionController.java
+++ bankingapp/src/main/java/com/dmt/bankingapp/controller/CommissionController.java
@@ -12,7 +12,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
